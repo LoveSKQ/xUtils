92c8a8278abb47f25c15f7a922dc4e412c6cbdd0
DZToXZT <loveskq@live.cn>
2014-06-18 16:49:33 +0800

    add ServiceStatus.java

--- src/com/rmj/xutils/app/Kill_Process.java
+++ src/com/rmj/xutils/app/Kill_Process.java
@@ -16,7 +16,9 @@ public class Kill_Process {
      * android.os.Process.killProcess(pid);
      * activityManager.killBackgroundProcesses(pkgName);
      * kill -9 pid
-     * 这三种方法都可以“杀死”后台应用程序，但是都会自启动，对于前面两种直接使用Android的API就行了，所以就不多介绍了。这里稍微介绍一下 最后一个方法的用法： kill -9 pid;
+     * 这三种方法都可以“杀死”后台应用程序，但是都会自启动
+     * 对于前面两种直接使用Android的API就行了，所以就不多介绍了。
+     * 这里稍微介绍一下 最后一个方法的用法： kill -9 pid;
      */
     public void method_1(String pid) {
             Process sh = null;
@@ -40,8 +42,9 @@ public class Kill_Process {
     }
 
     /**
-     *杀死后台service而不自启动： am （Activity Manager）命令
-     * 是Android系统中/system/bin/目录下的一条命令。不仅可以在终端下启动一个应用程序，还可以启动Service，发送 broadcast以及Intent action ，force stop process 等等，功能非常强大。这里我们要用到一个功能就是强制停止应用程序！
+     * 杀死后台service而不自启动： am （Activity Manager）命令是Android系统中/system/bin/目录下的一条命令。
+     * 不仅可以在终端下启动一个应用程序，还可以启动Service，发送 broadcast以及Intent action ，force stop process 等等，功能非常强大。
+     * 这里我们要用到一个功能就是强制停止应用程序！
      * 对于命令 am 的介绍与用法，官网给的非常清楚，请参照Android 官网： http://developer.android.com/tools/help/adb.html#am
      * 我们用到的方法和功能是：am force-stop <PACKAGE>
      */
@@ -54,12 +57,10 @@ public class Kill_Process {
                 final String Command = "am force-stop "+pkgName+ "\n";
                 os.writeBytes(Command);
                 os.flush();
-
             } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-
             try {
                 sh.waitFor();
             } catch (InterruptedException e) {
---
+++ src/com/rmj/xutils/service/ServiceStatus.java
@@ -0,0 +1,24 @@
+package com.rmj.xutils.service;
+
+import android.app.ActivityManager;
+import android.content.Context;
+
+import java.util.List;
+
+/**
+ * Created by G11 on 2014/6/18.
+ */
+public class ServiceStatus {
+    
+    public static boolean isServiceRunning(Context context,String serviename){
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> infos = am.getRunningServices(100);
+        for(ActivityManager.RunningServiceInfo info :infos){
+            String classname = info.service.getClassName();
+            if(serviename.equals(classname)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
